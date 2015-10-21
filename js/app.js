2c8c2822336a20eae4b868c39d1b2f99e9dec871
ThumbSnail <thumbsnail@zoho.com>
2015-10-21 10:27:37 -0700

    Feature:  Add basic player input
    And all the other basic pieces are testable/workable

--- js/app.js
+++ js/app.js
@@ -1,46 +1,142 @@
+//Input constants:
+var ALLOWED_KEYS = {
+	37: 'left',
+	38: 'up',
+	39: 'right',
+	40: 'down',
+	67: 'c'  //to switch player sprite
+};
+
+//Tile-Grid Dimension constants:
+var COL_WIDTH = 101;  //px
+var ROW_HEIGHT = 83;  //px
+var MAX_COL_INDEX = 4;	// 0 1 2 3 4
+var MAX_ROW_INDEX = 5;	// 1
+						// 2
+						// 3
+						// 4
+						// 5
+
+//Player Sprite constants:  (weird because so much transparency is saved in the image):
+var PLAYER_Y_OFFSET = -13;  //to center the player on a tile, shove up by this many px
+
+
+//Enemy Speed Range:
+var MAX_SPEED = 3;
+var MIN_SPEED = 1;
+
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
+	this.speed = Math.floor(Math.random() * (MAX_SPEED - MIN_SPEED) + MIN_SPEED); //Maybe Math.random() val?
+		//^Math.floor because Math.random returns a decimal between 0 and 1
+	this.spawn(); //should probably just pick a row and then be one col off left
+	//maybe the gems == could switch the direction the bugs run?
+	  //could you even make the game switch and have the bugs run down the column?
+};
+
+Enemy.prototype.spawn = function() {
+	//83 = the tile height  ...but why do the graphics have so much extra transparency?  That's gonna complicate all the positioning...
+	//101 = the tile width
+	this.x = -101; //0 - image width (101, unless there's a way to grab this property)
+	this.y = Math.floor(Math.random() * (3 - 1) + 1) * 83;//random row between 1 and 3 (0 is water, the rest is safe grass)  //IDEA:  maybe you could mix up the map upon restart?  like stone, grass, stone for the rows?
+
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+
+	//screen has 5 cols (0 - 4), 505px to work with.  So a set minimum speed for all might be 10px per update?
+	//and then enemies can have their own speed value of 1 thru 3, so the fastest ones move 30px per update
+	this.x += this.speed * 10 * dt;
+
+	//bounds:  once fully exits screen right == send back to left side of screen, but randomly switch rows.
+	if (this.x > (505 + 101)) {
+		this.x = -101;
+	}
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	this.sprite = 'images/char-boy.png';
+	this.col = 2;  //x
+	this.row = 5;  //y
+};
 
+
+Player.prototype.update = function(dt) {
+
+};
+
+//Draw the player on the screen.
+//Converts from tile position to x,y position
+Player.prototype.render = function() {
+	var x = this.col * COL_WIDTH;
+	var y = this.row * ROW_HEIGHT + PLAYER_Y_OFFSET;
+
+	ctx.drawImage(Resources.get(this.sprite), x, y);
+};
+
+//Using the arrow keys, move Player 1 tile from current position
+//Also prevents player from moving off the game screen
+//The 'c' key is used to swap character sprites
+Player.prototype.handleInput = function(key) {
+	switch(key) {
+		case 'left':
+			this.col > 0 ? this.col-- : this.col = 0;
+			break;
+		case 'right':
+			this.col < MAX_COL_INDEX ? this.col++ : this.col = MAX_COL_INDEX;
+			break;
+		case 'up':
+			this.row > 0 ? this.row-- : this.row = 0;
+			break;
+		case 'down':
+			this.row < MAX_ROW_INDEX ? this.row++ : this.row = MAX_ROW_INDEX;
+			break;
+	}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var MAX_ENEMIES = 10;
+var allEnemies = [];
+for (var i = 0; i < MAX_ENEMIES; i++) {
+	allEnemies.push(new Enemy());
+}
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+// Player.handleInput() method. You don't need to modify this.  (Well, I did!)
+document.addEventListener('keyup', function(event) {
+	player.handleInput(ALLOWED_KEYS[event.keyCode]);
+});
 
-    player.handleInput(allowedKeys[e.keyCode]);
+//This function stops keypresses from scrolling the page:
+document.addEventListener('keydown', function(event) {
+	for (var prop in ALLOWED_KEYS) {
+		if (event.keyCode === parseInt(prop)) {
+			event.preventDefault();
+		}
+	}
 });
--- js/resources.js
+++ js/resources.js
@@ -4,108 +4,125 @@
  * a simple "caching" layer so it will reuse cached images if you attempt
  * to load the same image multiple times.
  */
-(function() {
-    var resourceCache = {};
-    var loading = [];
-    var readyCallbacks = [];
+(function() {  //this must be for scope
+	var resourceCache = {};
+	var loading = [];
+	var readyCallbacks = [];
 
-    /* This is the publicly accessible image loading function. It accepts
-     * an array of strings pointing to image files or a string for a single
-     * image. It will then call our private image loading function accordingly.
-     */
-    function load(urlOrArr) {
-        if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
-             */
-            urlOrArr.forEach(function(url) {
-                _load(url);
-            });
-        } else {
-            /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
-             * directly.
-             */
-            _load(urlOrArr);
-        }
-    }
+	/* This is the publicly accessible image loading function. It accepts
+	 * an array of strings pointing to image files or a string for a single
+	 * image. It will then call our private image loading function accordingly.
+	 */
+	function load(urlOrArr) {
+		if(urlOrArr instanceof Array) {
+			/* If the developer passed in an array of images
+			 * loop through each value and call our image
+			 * loader on that image file
+			 */
+			urlOrArr.forEach(function(url) {
+				_load(url);
+			});
+		} else {
+			/* The developer did not pass an array to this function,
+			 * assume the value is a string and call our image loader
+			 * directly.
+			 */
+			_load(urlOrArr);
+		}
+	}
 
-    /* This is our private image loader function, it is
-     * called by the public image loader function.
-     */
-    function _load(url) {
-        if(resourceCache[url]) {
-            /* If this URL has been previously loaded it will exist within
-             * our resourceCache array. Just return that image rather
-             * re-loading the image.
-             */
-            return resourceCache[url];
-        } else {
-            /* This URL has not been previously loaded and is not present
-             * within our cache; we'll need to load this image.
-             */
-            var img = new Image();
-            img.onload = function() {
-                /* Once our image has properly loaded, add it to our cache
-                 * so that we can simply return this image if the developer
-                 * attempts to load this file in the future.
-                 */
-                resourceCache[url] = img;
+	/* This is our private image loader function, it is
+	 * called by the public image loader function.
+	 */
+	function _load(url) {
+		if(resourceCache[url]) {
+			/* If this URL has been previously loaded it will exist within
+			 * our resourceCache array. Just return that image rather
+			 * re-loading the image.
+			 */
+			return resourceCache[url];
+		} else {
+			/* This URL has not been previously loaded and is not present
+			 * within our cache; we'll need to load this image.
+			 */
+			var img = new Image();
 
-                /* Once the image is actually loaded and properly cached,
-                 * call all of the onReady() callbacks we have defined.
-                 */
-                if(isReady()) {
-                    readyCallbacks.forEach(function(func) { func(); });
-                }
-            };
+			//vOh, .onload is an Object event.
+			//Oooh, and why this is weird is because it's really just shorthand.
+			//.onload is short for:  object.addEventListener('load', callback);
+			//Thus, this function fires when the image finishes loading.
+			img.onload = function() {  
+				/* Once our image has properly loaded, add it to our cache
+				 * so that we can simply return this image if the developer
+				 * attempts to load this file in the future.
+				 */
+				resourceCache[url] = img;
 
-            /* Set the initial cache value to false, this will change when
-             * the image's onload event handler is called. Finally, point
-             * the images src attribute to the passed in URL.
-             */
-            resourceCache[url] = false;
-            img.src = url;
-        }
-    }
+				/* Once the image is actually loaded and properly cached,
+				 * call all of the onReady() callbacks we have defined.
+				 */
+				if(isReady()) {
+					readyCallbacks.forEach(function(func) { func(); });
+				}
+			};
 
-    /* This is used by developer's to grab references to images they know
-     * have been previously loaded. If an image is cached, this functions
-     * the same as calling load() on that URL.
-     */
-    function get(url) {
-        return resourceCache[url];
-    }
+			/* Set the initial cache value to false, this will change when
+			 * the image's onload event handler is called. Finally, point
+			 * the images src attribute to the passed in URL.
+			 */
+			resourceCache[url] = false;
+			img.src = url;
+		}
+	}
 
-    /* This function determines if all of the images that have been requested
-     * for loading have in fact been completed loaded.
-     */
-    function isReady() {
-        var ready = true;
-        for(var k in resourceCache) {
-            if(resourceCache.hasOwnProperty(k) &&
-               !resourceCache[k]) {
-                ready = false;
-            }
-        }
-        return ready;
-    }
+	/* This is used by developer's to grab references to images they know
+	 * have been previously loaded. If an image is cached, this functions
+	 * the same as calling load() on that URL.
+	 */
+	function get(url) {
+		return resourceCache[url];
+	}
 
-    /* This function will add a function to the callback stack that is called
-     * when all requested images are properly loaded.
-     */
-    function onReady(func) {
-        readyCallbacks.push(func);
-    }
+	/* This function determines if all of the images that have been requested
+	 * for loading have in fact been completed loaded.
+	 */
+	 //huh, I wonder why this is needed.  Oh, I see.  So, the images are loaded
+	 //randomly.  And each image.onload checks to see if ALL the images are ready.
+	 //Thus, this is needed to ensure that all of the images have actually indeed loaded.
+	 //ah, and the weird part--hasOwnProperty--is because a for-in loop loops thru
+	 //NOT just this object's properties but all the way up it's prototype chain.
+	 //I doubt it's actually needed in this particular function, but it's probably
+	 //a good practice just to include it for safety/expected logic.
+	function isReady() {
+		var ready = true;
+		for(var k in resourceCache) {
+			if(resourceCache.hasOwnProperty(k) &&
+			   !resourceCache[k]) {
+				ready = false;
+			}
+		}
+		return ready;
+	}
 
-    /* This object defines the publicly accessible functions available to
-     * developers by creating a global Resources object.
-     */
-    window.Resources = {
-        load: load,
-        get: get,
-        onReady: onReady,
-        isReady: isReady
-    };
-})();
+	/* This function will add a function to the callback stack that is called
+	 * when all requested images are properly loaded.
+	 */
+	function onReady(func) {
+		readyCallbacks.push(func);
+	}
+
+	/* This object defines the publicly accessible functions available to
+	 * developers by creating a global Resources object.
+	 */
+	 //Ah, you can just declare a global variable without window and by leaving off
+	 //the var keyword.  HOWEVER, using window lets people know that you actually
+	 //INTEND for this to be global.  If you just leave out var, it could easily
+	 //be interpretted as a typo mistake.
+	window.Resources = {
+		load: load,
+		get: get,
+		onReady: onReady,
+		isReady: isReady
+	};
+})();  //this must mean to call it as soon as the script loads (because otherwise,
+		//you'd have no way to access any of this.)
